65da6f2614528fca76a3ecd814c2f2374c930326
Pattol Albadry <71397580+Pattol@users.noreply.github.com>
2021-01-15 20:56:03 -0500

    deleted

--- lab10/task3.py
+++
@@ -1,9 +0,0 @@
-import csv
-
-with open('font3.txt', 'r') as in_file:
-    stripped = (line.strip() for line in in_file)
-    lines = (line.split(",") for line in stripped if line)
-    with open('font3.csv', 'w') as out_file:
-        writer = csv.writer(out_file)
-        writer.writerow(('value', 'Key'))
-        writer.writerows(lines)
